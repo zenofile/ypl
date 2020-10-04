0ac0a97f3d33db2e770d79abc5034c363bfdc6e7
Thorsten Schubert <tschubert@bafh.org>
2020-10-04 02:53:33 +0200

    Use a generator

--- ydlp.py
+++ ydlp.py
@@ -1,91 +1,129 @@
+from __future__ import annotations
+
+import argparse
 import os
+import pickle
+import pprint
 import stat
 import sys
-import pickle
-import argparse
-import pprint
-import google.oauth2.credentials
-
+import typing
 from contextlib import suppress
-from typing import Iterator, Set, List, Tuple, Callable
 
-from google.auth.transport.requests import Request
-from googleapiclient.discovery import build, Resource
-from googleapiclient.errors import HttpError
+from google.auth.transport.requests import Request as ApiRequest
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import Resource as GApiResource
+from googleapiclient.discovery import build as BuildResource
+from googleapiclient.errors import HttpError
 
-CLIENT_SECRETS_FILE = 'client_secret.json'
-SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
-API_SERVICE_NAME = 'youtube'
-API_VERSION = 'v3'
+if typing.TYPE_CHECKING:
+    from io import TextIO
+    from typing import Callable, Generator, Iterator, Tuple
+
+    from googleapiclient._apis.youtube.v3.resources import PlaylistItemsResource
+
+CLIENT_SECRETS_FILE = "client_secret.json"
+SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+API_SERVICE_NAME = "youtube"
+API_VERSION = "v3"
 
 
-def authenticate(headless: bool = True) -> Resource:
+def authenticate(headless: bool = True) -> GApiResource:
     creds = None
-    if os.path.exists('.token'):
-        with open('.token', 'rb') as token:
+
+    if os.path.exists(".token"):
+        with open(".token", "rb") as token:
             creds = pickle.load(token)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            creds.refresh(ApiRequest())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                CLIENT_SECRETS_FILE, SCOPES)
-            if not headless:
-                credentials = flow.run_local_server()
-            else:
-                credentials = flow.run_console()
-            creds = flow.run_local_server(port=0)
-        with open('.token', 'wb') as token:
+                CLIENT_SECRETS_FILE, SCOPES
+            )
+            creds = flow.run_console() if headless else flow.run_local_server(port=0)
+
+        with open(".token", "wb") as token:
             with suppress(OSError):
                 os.fchmod(token.fileno(), stat.S_IRUSR | stat.S_IWUSR)
             pickle.dump(creds, token)
-    return build(API_SERVICE_NAME, API_VERSION, credentials=creds)
+
+    return BuildResource(API_SERVICE_NAME, API_VERSION, credentials=creds)
 
 
 def enum_vids(
     id: str,
     page_size: int = 50,
     verbose: bool = False,
-    printer: Callable[..., None] = pprint.PrettyPrinter(indent=4).pprint
-) -> List[Iterator]:
+    printer: Callable[..., None] = pprint.PrettyPrinter(indent=4).pprint,
+) -> Generator[str, None, None]:
     api = authenticate(headless=False)
     if api:
-        request = api.playlistItems().list(part="snippet",
-                                           playlistId=id,
-                                           maxResults=page_size)
+        request = api.playlistItems().list(
+            part="snippet", playlistId=id, maxResults=page_size
+        )
+
         while request:
-            response = request.execute()
-            if verbose:
-                printer(response)
-            
-            yield map(lambda l: l['snippet']['resourceId']['videoId'],
-                      response['items'])
-            request = api.playlistItems().list_next(request, response)
+            tries = 1
+            try:
+                response = request.execute()
+                if verbose:
+                    printer(response)
+                for item in response["items"]:
+                    yield item["snippet"]["resourceId"]["videoId"]
+                request = api.playlistItems().list_next(request, response)
+
+            except HttpError as err:
+                # probably rate limiting, try one more time
+                if err.resp.status in (403, 500, 503) and tries > 0:
+                    import time
+
+                    time.sleep(5)
+                    tries -= 1
+                else:
+                    print(
+                        "There was an error creating the model. Check the details:\n{}".format(
+                            err._get_reason()
+                        ),
+                        file=sys.stderr,
+                    )
+                    raise
+
+
+def print_iter(
+    it: Iterator,
+    fmt: str,
+    surround: Tuple[str, str] = ("\n", "\n"),
+    out: TextIO = sys.stderr,
+):
+    print(surround[0], file=sys.stderr)
+    for v in it:
+        print(fmt.format(v))
+    print(surround[1], file=sys.stderr)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        prog='ydlp', description='Lists all videos inside a playlist.')
-    parser.add_argument('id', type=str, help='The playlist id.')
-    parser.add_argument('-v',
-                        '--verbose',
-                        dest='verbose',
-                        help='increase output verbosity',
-                        action='store_true')
+        prog="ydlp", description="Lists all videos inside a playlist."
+    )
+    parser.add_argument("id", type=str, help="The playlist id.")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        help="increase output verbosity",
+        action="store_true",
+    )
     args = parser.parse_args()
-    # build set from list of map iterators and prepend URI prefix
-    if args.verbose:
-        print('\n' * 2, file=sys.stderr)
-    vids = {e for m in enum_vids(args.id, verbose=args.verbose) for e in m}
-    if args.verbose:
-        print('\n' * 2, file=sys.stderr)
-    for vid in vids:
-        print('https://www.youtube.com/watch?v={}'.format(vid))
-    print('\nExtracted {} elements.'.format(len(vids)), file=sys.stderr)
+    vids = {vid for vid in enum_vids(args.id, verbose=args.verbose)}
+    print_iter(
+        iter(vids),
+        "https://www.youtube.com/watch?v={}",
+        ("\n", "\n") if args.verbose else (str(), str()),
+    )
+    print("Extracted {} elements.".format(len(vids)), file=sys.stderr)
 
 
 if __name__ == "__main__":
     main()
 
-#EOF
+# EOF
